8dace33e4f83e4c420a2ac34b43c2ff1e6eb7692
ZeroDay0utplay <102672424+ZeroDay0utplay@users.noreply.github.com>
2022-08-22 19:53:37 +0100

    Delete heroku_pusher.py

--- heroku_pusher.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-os.system("heroku login")
-os.system("git add .")
-os.system("git commit -m '" + input("[+] Commit Message : ") + '"')
-os.system("heroku create")
-os.system("git push heroku master")
-os.system("PAUSE")
