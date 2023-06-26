6b305ac5a5d1f37b926cec44149cc64aeec3b564
zdo <hama.tm662@gmail.com>
2023-06-26 19:15:07 +0100

    replace 403 with login redirection

--- exports/auth.js
+++ exports/auth.js
@@ -9,14 +9,14 @@ function generateAccessToken(username) {
 const authorization = (req, res, next) => {
     const token = req.cookies.access_token;
     if (!token) {
-      return res.sendStatus(403);
+      return res.render("../views/sign_in",  {login: ""});
     }
     try {
         const data = jwt.verify(token, process.env.TOKEN_SECRET);
         req.phone_number = data.phone_number;
         return next();
     } catch {
-        return res.sendStatus(403);
+        return res.render("../views/sign_in",  {login: ""});
     }
 };
 
